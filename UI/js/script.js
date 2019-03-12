6b1b190e61a24d28c08138ed2f6c2baa748d5c2f
Amaobi Victor <aobikobe@gmail.com>
2019-03-12 21:16:59 +0100

    fix(api): non responsive display of ui
    - add cancel button for removal of side bar
    - align picture and user properly
    - modify the font of the website for logo
    - refactor ui/js/script.js
    
    [Delivers 164541307]

--- UI/js/script.js
+++ UI/js/script.js
@@ -2,58 +2,23 @@ class DivSelector {
   constructor() {
     /** Initializing Left Tabs */
     this.currentDivName = 'inbox_list';
-    this.composeTab = document.getElementById('compose_tab');
-    this.inboxTab = document.getElementById('inbox_tab');
-    this.sentTab = document.getElementById('sent_tab');
-    this.draftTab = document.getElementById('draft_tab');
-    this.groupTab = document.getElementById('group_tab');
-    this.editDPTab = document.getElementById('editDP');
-    this.logoutTab = document.getElementById('logout');
+    this.elementInitialization();
+    this.fabElementInitialization();
+    this.handlersInitialization();
 
-    this.menuBar = document.getElementById('menu_button');
-    this.changePasswordTab = document.getElementById('change_password_button');
-    this.viewComposeForGroups = document.querySelectorAll(
-      '#listview_group > a',
-    );
-    this.viewInboxMessage = document.querySelectorAll('#inbox_list > div > a');
-
-
-    /** Initailizing tab for admin */
-    this.adminGroupTab = document.getElementById('admin_group_tab');
-
-    /** Initializing list of user view for admin */
-    this.viewMembersOfGrroupForAdmin = document.querySelectorAll(
-      '#admin_group > a',
-    );
-
-    this.adminGroupDiv = document.getElementById('admin_group'); // list of all groups for admin
-    this.listOfUsersDiv = document.getElementById('list_of_users'); // list of users in a group for admin
-    this.creatGroupDiv = document.getElementById('create_group');
-    this.userProfileDiv = document.getElementById('user-profile');
-
-    /** Attaching Event Listeners */
-    this.composeTab.addEventListener('click', () => this.onComposeClicked());
-    this.inboxTab.addEventListener('click', () => this.onInboxClicked());
-    this.sentTab.addEventListener('click', () => this.onSentClicked());
-    this.draftTab.addEventListener('click', () => this.onDraftClicked());
-    this.groupTab.addEventListener('click', () => this.onGroupClicked());
-    this.menuBar.addEventListener('click', () => this.onMenuBarClicked());
-    for (const group of this.viewComposeForGroups) {
-      group.addEventListener('click', () => this.onComposeGroupClicked());
+    this.retractButton.display = 'none';
+    /** On mobile side bar */
+    if (window.matchMedia('(max-width: 700px)').matches) {
+      this.editDPlink = document.getElementById('editDPlink');
+      this.passwordlink = document.getElementById('passwordlink');
+      this.logoutlink = document.getElementById('logoutlink');
+      this.editDPlink.addEventListener('click', () => this.onEditDpClicked());
+      this.logoutlink.addEventListener('click', () => this.onLogOutClicked());
+      this.passwordlink.addEventListener('click', () => this.onChangePassWordClicked());
     }
+  }
 
-    for (const group of this.viewMembersOfGrroupForAdmin) {
-      group.addEventListener('click', () => this.onViewMembersOfGrroupForAdminClicked());
-    }
-
-    for (const group of this.viewInboxMessage) {
-      group.addEventListener('click', () => this.onViewInboxMessageClicked());
-    }
-
-    this.adminGroupTab.addEventListener('click', () => this.onAdminGroupClicked());
-    document
-      .querySelector('main')
-      .addEventListener('click', () => this.onSideBarClicked());
+  fabElementInitialization() {
     /** Floating Action Buttons (FAB) */
 
     this.sendFAB = document.getElementById('send');
@@ -68,16 +33,6 @@ class DivSelector {
 
     /** Retract button in sent items list view */
     this.retractButton = document.querySelector('button.retract');
-    this.retractButton.display = 'none';
-
-
-    /** Title for sent, inbox and draft (Listview) */
-    this.inboxTitle = document.querySelector('#inbox_list h1');
-
-    /** Title for sent, inbox and draft (single message) */
-    this.msgTitle = document.querySelector('#inbox_msg h1');
-
-    this.emailField = document.getElementById('email');
 
     this.fab = [
       this.sendFAB,
@@ -87,58 +42,131 @@ class DivSelector {
       this.plusFAB,
       this.createFAB,
     ];
+  }
 
+  elementInitialization() {
+    this.composeTab = document.getElementById('compose_tab');
+    this.inboxTab = document.getElementById('inbox_tab');
+    this.sentTab = document.getElementById('sent_tab');
+    this.draftTab = document.getElementById('draft_tab');
+    this.groupTab = document.getElementById('group_tab');
+    this.editDPTab = document.getElementById('editDP');
+    this.logoutTab = document.getElementById('logout');
+
+    this.menuBar = document.getElementById('menu_button');
+    this.closeDiv = document.getElementById('close-div');
+    this.closeButton = document.querySelector('.close-div > button');
+    this.changePasswordTab = document.getElementById('change_password_button');
+    this.viewComposeForGroups = document.querySelectorAll(
+      '#listview_group > a',
+    );
+    this.viewInboxMessage = document.querySelectorAll('#inbox_list > div > a');
+    /** Initailizing tab for admin */
+    this.adminGroupTab = document.getElementById('admin_group_tab');
+
+    /** Initializing list of user view for admin */
+    this.viewMembersOfGrroupForAdmin = document.querySelectorAll(
+      '#admin_group > a',
+    );
+
+    this.adminGroupDiv = document.getElementById('admin_group'); // list of all groups for admin
+    this.listOfUsersDiv = document.getElementById('list_of_users'); // list of users in a group for admin
+    this.creatGroupDiv = document.getElementById('create_group');
+    this.userProfileDiv = document.getElementById('user-profile');
+
+    /** Title for sent, inbox and draft (Listview) */
+    this.inboxTitle = document.querySelector('#inbox_list h1');
+
+    /** Title for sent, inbox and draft (single message) */
+    this.msgTitle = document.querySelector('#inbox_msg h1');
+
+    this.emailField = document.getElementById('email');
+  }
+
+  handlersInitialization() {
+    /** Attaching Event Listeners */
+    this.composeTab.addEventListener('click', () => this.onComposeClicked());
+    this.inboxTab.addEventListener('click', () => this.onInboxClicked());
+    this.sentTab.addEventListener('click', () => this.onSentClicked());
+    this.draftTab.addEventListener('click', () => this.onDraftClicked());
+    this.groupTab.addEventListener('click', () => this.onGroupClicked());
+    this.menuBar.addEventListener('click', () => this.onMenuBarClicked());
+    this.viewComposeForGroups.forEach((group) => {
+      group.addEventListener('click', () => this.onComposeGroupClicked());
+    });
+    this.viewMembersOfGrroupForAdmin.forEach((group) => {
+      group.addEventListener('click', () => this.onViewMembersOfGrroupForAdminClicked());
+    });
+    this.viewInboxMessage.forEach((group) => {
+      group.addEventListener('click', () => this.onViewInboxMessageClicked());
+    });
+
+    this.adminGroupTab.addEventListener('click', () => this.onAdminGroupClicked());
+    document
+      .querySelector('main')
+      .addEventListener('click', () => this.onSideBarClicked());
+
+    this.closeButton.addEventListener('click', () => {
+      this.onClosedBarClicked();
+    });
 
     this.editDPTab.addEventListener('click', () => this.onEditDpClicked());
     this.logoutTab.addEventListener('click', () => this.onLogOutClicked());
     this.changePasswordTab.addEventListener('click', () => this.onChangePassWordClicked());
-
-
-    if (window.matchMedia('(max-width: 700px)').matches) {
-      this.editDPlink = document.getElementById('editDPlink');
-      this.passwordlink = document.getElementById('passwordlink');
-      this.logoutlink = document.getElementById('logoutlink');
-      this.editDPlink.addEventListener('click', () => this.onEditDpClicked());
-      this.logoutlink.addEventListener('click', () => this.onLogOutClicked());
-      this.passwordlink.addEventListener('click', () => this.onChangePassWordClicked());
-    }
   }
 
-  onLogOutClicked() {
+  static onLogOutClicked() {
     window.location = '../index.html';
   }
 
   onEditDpClicked() {
     this.onFABRemoveAll();
-
     this.saveFAB.classList.remove('no-display');
-    this.changeDivTo('user_profile');
+    this.onTopNavClicked('user_profile');
   }
 
   onChangePassWordClicked() {
+    this.onTopNavClicked('change_password');
+  }
+
+  onTopNavClicked(action) {
     this.onFABRemoveAll();
 
     this.saveFAB.classList.remove('no-display');
-    this.changeDivTo('change_password');
+    this.changeDivTo(action);
   }
 
   onMenuBarClicked() {
     document.querySelector('aside').style.display = 'block';
+
+    this.closeDiv.style.display = 'block';
+    this.menuBar.style.display = 'none';
+  }
+
+  onClosedBarClicked() {
+    this.hidesideBar();
   }
 
   /** Remove sideBar using viewport */
   onSideBarClicked() {
     if (window.matchMedia('(max-width: 700px)').matches) {
-      document.querySelector('aside').style.display = 'none';
+      this.hidesideBar();
     }
+  }
+
+  hidesideBar() {
+    document.querySelector('aside').style.display = 'none';
+
+    this.closeDiv.style.display = 'none';
+    this.menuBar.style.display = 'block';
   }
 
   /** Remove all FAB */
 
   onFABRemoveAll() {
-    for (const fab of this.fab) {
+    this.fab.forEach((fab) => {
       fab.classList.add('no-display');
-    }
+    });
   }
 
   /** Event Listeners For User Tab */
@@ -163,18 +191,18 @@ class DivSelector {
   }
 
   onSentClicked() {
-    this.onFABRemoveAll();
-    this.listState = 'sent';
-    this.inboxTitle.innerHTML = 'Sent Items';
-    this.msgTitle.innerHTML = 'Mail To';
-    this.changeDivTo('inbox_list');
+    this.onMessageViewChange('sent', 'Sent Items', 'Mail To');
   }
 
   onDraftClicked() {
+    this.onMessageViewChange('draft', 'Draft', '');
+  }
+
+  onMessageViewChange(state, page, innerTitle) {
     this.onFABRemoveAll();
-    this.listState = 'draft';
-    this.inboxTitle.innerHTML = 'Draft';
-    this.msgTitle.innerHTML = '';
+    this.listState = state;
+    this.inboxTitle.innerHTML = page;
+    this.msgTitle.innerHTML = innerTitle;
     this.changeDivTo('inbox_list');
   }
 
@@ -208,20 +236,16 @@ class DivSelector {
   }
 
   onViewAddUserToGroupClicked() {
-    this.onFABRemoveAll();
-    document.querySelector('#create_group h1').innerHTML = 'Add User';
-    document.querySelector('#create_group h2').classList.remove('no-display');
-    document.querySelector('#create_group ul').classList.remove('no-display');
-    document
-      .querySelector('#create_group #group-name')
-      .classList.add('no-display');
-    this.saveFAB.classList.remove('no-display');
-    this.changeDivTo('create_group');
+    this.changeGroupView('Add User');
   }
 
   onViewCreateGroupClicked() {
+    this.changeGroupView('Create Group');
+  }
+
+  changeGroupView(header) {
     this.onFABRemoveAll();
-    document.querySelector('#create_group h1').innerHTML = 'Create Group';
+    document.querySelector('#create_group h1').innerHTML = header;
     document.querySelector('#create_group h2').classList.add('no-display');
     document.querySelector('#create_group ul').classList.add('no-display');
     document
