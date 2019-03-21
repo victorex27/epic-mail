1d66f44a203c717b060f1ebf8e0b94be5595d2b9
Amaobi Victor <aobikobe@gmail.com>
2019-03-21 10:49:00 +0100

    console out in travis

--- src/helpers/query.js
+++ src/helpers/query.js
@@ -6,11 +6,14 @@ class Query {
       const { rows } = await db.query(text, values);
       return rows;
     } catch (error) {
+
+      
       // @return 1 signifies Duplicate entry
 
       if (error.routine === '_bt_check_unique') {
         return 1;
       }
+      console.log(error);
       return 2; // other types of errors
     }
   }
