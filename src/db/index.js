e990b5697c3152e949837791fe06fe52a787f211
Amaobi Victor <aobikobe@gmail.com>
2019-03-22 05:40:57 +0100

    change settings

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,7 @@ import 'idempotent-babel-polyfill'; // for babel not to be called twice
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL_PROD,
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default {
