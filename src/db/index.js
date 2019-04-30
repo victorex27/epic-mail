d3825f2bf1679847d3365798157b7398960dccb4
Amaobi Victor <aobikobe@gmail.com>
2019-04-30 02:51:05 +0100

    complete user able to send message for version 2

--- src/db/index.js
+++ src/db/index.js
@@ -6,7 +6,7 @@ import 'idempotent-babel-polyfill'; // for babel not to be called twice
 
 dotenv.config();
 
-let connectionUrl = process.env.DATABASE_URL;
+let connectionUrl = process.env.DATABASE_URL_DEV;
 let initializerQuery = userTable + messageTable + userSeed;
 
 if (process.env.NODE_ENV === 'production') {
